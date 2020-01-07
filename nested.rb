09a62df9de5b098aa9aeebed48ee75d4b7c672c9
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 01:26:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash
-
+  programmer_hash[:yukihiro_matsumoto] ={ :known_for =>"Ruby", :languages => ["LISP", "C"] }
+  return programmer_hash
 end
 
 def changing_alan
